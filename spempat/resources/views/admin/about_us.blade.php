87cd8907ac8fbd921670d3ee5d301391dfd9716e
Marselino33 <151488147+IvanGunawanSimanjuntak@users.noreply.github.com>
2024-06-04 21:14:21 +0700

    ok

--- spempat/resources/views/admin/about_us.blade.php
+++ spempat/resources/views/admin/about_us.blade.php
@@ -1,87 +1,87 @@
 @extends('layouts.main')
 @section('content')
-@foreach ($data as $item)
-    
+    @foreach ($data as $item)
+        <table class="table table-border  caption-top shadow">
+            <caption>
+                <h1>Tentang Kami</h1>
+            </caption>
+            <tr>
+                <th class="col-md-6 table-secondary">Email</tj>
+                <td class="col-md-6 ">{{ $item->email }}</td>
+            </tr>
+            <tr>
+                <th class="col-md-6 table-secondary">Alamat</tj>
+                <td class="col-md-6 ">{{ $item->alamat }}</td>
+            </tr>
+            <tr>
+                <th class="col-md-6 table-secondary">No.Telepon</tj>
+                <td class="col-md-6 ">{{ $item->no_telepon }}</td>
+            </tr>
 
-<table class="table table-border  caption-top shadow">
-    <caption>
-        <h1>Tentang Kami</h1>
-    </caption>
-    <tr>
-        <th class="col-md-6 table-secondary">Email</tj>
-        <td class="col-md-6 ">{{ $item->email }}</td>
-    </tr>
-    <tr>
-        <th class="col-md-6 table-secondary">Alamat</tj>
-        <td class="col-md-6 ">{{ $item->alamat }}</td>
-    </tr>
-    <tr>
-        <th class="col-md-6 table-secondary">No.Telepon</tj>
-        <td class="col-md-6 ">{{ $item->no_telepon }}</td>
-    </tr>
-    
-    
-    <tr>
-        <th class="col-md-6 table-secondary">Aksi </th>
-        <td class="col-md-6 ">
-            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#{{ 'staticBackdropeditaboutus' . $item->id }}">
-                <i class="lni lni-pencil"></i>
-            </button>
-        </td>
-    </tr>
-</table>
-@endforeach
-@foreach ($data as $item)
-    <div class="modal fade" id={{ 'staticBackdropeditaboutus' . $item->id }} data-bs-backdrop="static"
-        data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Edit About Us</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('about_us.update',['id'=>$item->id]) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="card-body">
-                            <div class="form-group">
-                                
+
+            <tr>
+                <th class="col-md-6 table-secondary">Aksi </th>
+                <td class="col-md-6 ">
+                    <button type="button" class="btn btn-warning" data-bs-toggle="modal"
+                        data-bs-target="#{{ 'staticBackdropeditaboutus' . $item->id }}">
+                        <i class="lni lni-pencil"></i>
+                    </button>
+                </td>
+            </tr>
+        </table>
+    @endforeach
+    @foreach ($data as $item)
+        <div class="modal fade" id={{ 'staticBackdropeditaboutus' . $item->id }} data-bs-backdrop="static"
+            data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Edit Tentang Kami</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('about_us.update', ['id' => $item->id]) }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-body">
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Email</label>
-                                    <textarea name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter content">{{ $item->email }}</textarea>
 
-                                    @error('email')
-                                        <small style="color: brown">{{ $message }}</small>
-                                    @enderror
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email</label>
+                                        <textarea name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter content">{{ $item->email }}</textarea>
+
+                                        @error('email')
+                                            <small style="color: brown">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Alamat</label>
+                                        <textarea name="alamat" class="form-control" id="exampleInputEmail1" placeholder="Enter content">{{ $item->alamat }}</textarea>
+
+                                        @error('alamat')
+                                            <small style="color: brown">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">No Telepon</label>
+                                        <textarea name="no_telepon" class="form-control" id="exampleInputEmail1" placeholder="Enter content">{{ $item->no_telepon }}</textarea>
+
+                                        @error('no_telepon')
+                                            <small style="color: brown">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+
                                 </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Alamat</label>
-                                    <textarea name="alamat" class="form-control" id="exampleInputEmail1" placeholder="Enter content">{{ $item->alamat }}</textarea>
-
-                                    @error('alamat')
-                                        <small style="color: brown">{{ $message }}</small>
-                                    @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">No Telepon</label>
-                                    <textarea name="no_telepon" class="form-control" id="exampleInputEmail1" placeholder="Enter content">{{ $item->no_telepon }}</textarea>
-
-                                    @error('no_telepon')
-                                        <small style="color: brown">{{ $message }}</small>
-                                    @enderror
-                                </div>
-
                             </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                        </div>
-                    </form>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
 @endsection
--- spempat/resources/views/show_berita.blade.php
+++ spempat/resources/views/show_berita.blade.php
@@ -1,35 +1,32 @@
 @extends('layouts_user.main')
 @section('content')
-
-<div class="container-fluid hero">
-    <h1>BERITA TERKINI</h1>
-    <div class="row gambar">
-        <div class="col-4 col-md-4">
-            <img src="{{ asset('img/1.jpg') }}" width="500" height="500">
+    <div class="container-fluid hero">
+        <h1>BERITA TERKINI</h1>
+        <div class="row gambar">
+            <div class="col-4 col-md-4">
+                <img src="gambar/data1.jpg" width="500" height="500">
+            </div>
+            <div class="col-4 col-md-4">
+                <img src="gambar/sejarah.jpg" width="500" height="500">
+            </div>
+            <div class="col-4 col-md-4">
+                <img src="gambar/data2.jpg" width="500" height="500">
+            </div>
         </div>
-        <div class="col-4 col-md-4">
-            <img src="{{ asset('img/2.jpg') }}" width="500" height="500">
+
+        <div class="list">
         </div>
-        <div class="col-4 col-md-4">
-            <img src="{{ asset('img/3.jpg') }}" width="500" height="500">
+
+        <!--Tampilan isi berita-->
+        <div class="container-fluid text-center news">
+            <h2>{{ $berita->judul }}</h2>
+            <img src="{{ asset('storage/img/' . $berita->file) }}" width="800" height="400">
+            <h6>Tanggal publish: {{ $berita->created_at }}</h6>
+            <div class="container-fluid">
+                <p>{{ $berita->isi }} </p>
+            </div>
         </div>
-    </div>
-
-    <div class="list">
-    </div>
-
-    <!--Tampilan isi berita-->
-    <div class="container-fluid text-center news">
-        <h1>{{ $berita->judul }}</h1>
-        <img class="shadow" src="{{ asset('storage/images/'. $berita->file) }}" width="800" height="400">
-        
-        <div class="container-fluid">
-            <p>{{ $berita->isi }} </p>
-
-        </div>
-        <h2>Tanggal publish: {{ $berita->created_at }}</h2>
-    </div>
 
 
-@endsection
-   
+        <!--Tampilan isi berita-->
+    @endsection
